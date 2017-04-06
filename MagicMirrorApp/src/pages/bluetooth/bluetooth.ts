4ed60aed6a7e8eb745b70285a43c56e5e4926ff6
crispybee <crispybee9@gmail.com>
2017-04-06 17:13:34 +0200

    add modal with bluetooth device search + refresher

--- MagicMirrorApp/src/pages/bluetooth/bluetooth.ts
+++ MagicMirrorApp/src/pages/bluetooth/bluetooth.ts
@@ -1,15 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import { Platform, NavParams, ViewController } from 'ionic-angular';
+import { BLE } from '@ionic-native/ble';
+
+export class BluetoothDevice {
+    constructor(public deviceName: string, private macAddress: string) { }
+}
 
 @Component({
   selector: 'page-bluetooth',
   templateUrl: 'bluetooth.html'
 })
 export class BluetoothPage {
+  scannedBluetoothDevices: BluetoothDevice[] = [];
 
-  constructor(public platform: Platform, public params: NavParams, public viewController: ViewController) { }
+  constructor(public platform: Platform, public params: NavParams, public viewController: ViewController, private ble: BLE, private ref: ChangeDetectorRef) {
+      this.scanBluetoothDevices();
+  }
+
+  scanBluetoothDevices() {
+      this.ble.startScan([]).subscribe(device => {
+      console.log(JSON.stringify(device));
+      this.scannedBluetoothDevices.push(new BluetoothDevice(device.name, device.id));
+
+      // Fix for *ngFor bug
+      this.ref.detectChanges();
+      console.log("scannedBluetoothDevices", this.scannedBluetoothDevices);
+      }, error => {
+          console.log(error);
+      });
+  }
+    
+  doRefresh(refresher) {
+    console.log('Begin async operation', refresher);
+
+    this.ble.stopScan();
+    this.scannedBluetoothDevices = [];
+    this.scanBluetoothDevices();
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      refresher.complete();
+    }, 100);
+  }
 
   dismiss() {
+    this.ble.stopScan();
     this.viewController.dismiss();
   }
 }
--- MagicMirrorApp/src/pages/home/home.ts
+++ MagicMirrorApp/src/pages/home/home.ts
@@ -8,8 +8,7 @@ import { ModalController, NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public modalCtrl: ModalController, public navCtrl: NavController) {  }
+  constructor(public modalCtrl: ModalController, public navCtrl: NavController) { }
 
   openModal() {
       let modal = this.modalCtrl.create(BluetoothPage);
