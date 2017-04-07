8534f8bfb3ae39ce6470fe30de69a864d9986b12
crispybee <crispybee9@gmail.com>
2017-04-07 13:19:21 +0200

    startup: check if bluetooth is enabled and show devices or warning

--- MagicMirrorApp/src/app/app.component.ts
+++ MagicMirrorApp/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, ModalController, AlertController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
+import { BluetoothPage } from '../pages/bluetooth/bluetooth';
 import { BLE } from '@ionic-native/ble';
 
 
@@ -13,13 +14,35 @@ import { BLE } from '@ionic-native/ble';
 export class MyApp {
   rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, ble: BLE) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, ble: BLE, private modalCtrl: ModalController, public alertCtrl: AlertController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
-      ble.enable();
+
+      ble.enable().then(enable => {
+        console.log(enable);
+        this.openModal();
+      }).catch(error => {
+        console.log(error);
+        this.showAlert();
+      });
     });
+  }
+
+  openModal() {
+      let modal = this.modalCtrl.create(BluetoothPage);
+      modal.present();
+  }
+
+  showAlert() {
+      let alert = this.alertCtrl.create({
+        title: "Enable Bluetooth",
+        subTitle: "Please enable Bluetooth on your phone to connect to the Magic Mirror.",
+        buttons: ["OK"]
+    });
+
+    alert.present();
   }
 }
