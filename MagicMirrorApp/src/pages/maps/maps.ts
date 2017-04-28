252ec3926b9f25c9e0800d0ca09e3276216a9533
Jenshika Yoganathan <jyoganat@stud.hs-heilbronn.de>
2017-04-28 19:02:39 +0200

    Fixed problems with googlemaps-api, added wifi-network-name to settings

--- MagicMirrorApp/src/pages/maps/maps.ts
+++ MagicMirrorApp/src/pages/maps/maps.ts
@@ -1,8 +1,16 @@
 import {Component, ViewChild, ElementRef} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import {NavController, Platform} from 'ionic-angular';
 import {Geolocation} from '@ionic-native/geolocation';
-
-declare var google;
+// import {
+//   GoogleMaps,
+//   GoogleMap,
+//   GoogleMapsEvent,
+//   LatLng,
+//   CameraPosition,
+//   MarkerOptions,
+//   Marker
+// } from '@ionic-native/google-maps';
+import { GoogleMap, GoogleMapsEvent, LatLng } from '@ionic-native/google-maps';
 
 @Component({
   selector: 'page-maps',
@@ -10,58 +18,44 @@ declare var google;
 })
 export class MapPage {
 
-  @ViewChild('map') mapElement: ElementRef;
-  map: any;
 
-  constructor(public navCtrl: NavController, public geolocation: Geolocation) {
+  map: GoogleMap;
 
+  constructor(public navCtrl: NavController, public platform: Platform,private geolocation: Geolocation) {
+    platform.ready().then(() => {
+      this.loadMap();
+    });
   }
 
-  ionViewDidLoad() {
-    this.loadMap();
-  }
+  loadMap(){
 
-  loadMap() {
+    let location = new LatLng(-34.9290,138.6010);
 
-    this.geolocation.getCurrentPosition().then((position) => {
-
-      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-
-      let mapOptions = {
-        center: latLng,
-        zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+    this.map = new GoogleMap('map', {
+      'backgroundColor': 'white',
+      'controls': {
+        'compass': true,
+        'myLocationButton': true,
+        'indoorPicker': true,
+        'zoom': true
+      },
+      'gestures': {
+        'scroll': true,
+        'tilt': true,
+        'rotate': true,
+        'zoom': true
+      },
+      'camera': {
+        'latLng': location,
+        'tilt': 30,
+        'zoom': 15,
+        'bearing': 50
       }
-
-      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-
-    }, (err) => {
-      console.log(err);
     });
-  }
+    this.map.setCenter(location);
 
-  addMarker() {
-
-    let marker = new google.maps.Marker({
-      map: this.map,
-      animation: google.maps.Animation.DROP,
-      position: this.map.getCenter()
-    });
-
-    let content = "<h4>Information!</h4>";
-
-    this.addInfoWindow(marker, content);
-
-  }
-
-  addInfoWindow(marker, content) {
-
-    let infoWindow = new google.maps.InfoWindow({
-      content: content
-    });
-
-    google.maps.event.addListener(marker, 'click', () => {
-      infoWindow.open(this.map, marker);
+    this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
+      console.log('Map is ready!');
     });
 
   }
