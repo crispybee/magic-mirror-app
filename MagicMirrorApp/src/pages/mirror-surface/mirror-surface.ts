d406329dc2a1a14a12ae18243973434e44408118
crispybee <crispybee9@gmail.com>
2017-05-02 01:41:16 +0200

    add basic draggable packery grid

--- MagicMirrorApp/src/pages/mirror-surface/mirror-surface.ts
+++ MagicMirrorApp/src/pages/mirror-surface/mirror-surface.ts
@@ -1,17 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import * as Packery from 'packery';
+import * as Draggabilly from 'draggabilly';
 
 @Component({
   selector: 'page-mirror-surface',
   templateUrl: 'mirror-surface.html'
 })
-export class MirrorSurfacePage {
+export class MirrorSurfacePage implements AfterViewInit {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  @ViewChild('packeryGrid') grid: ElementRef;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad MirrorSurfacePage');
+  doSomething(event) {
+    // event.target.classList.toggle('grid-item--width2');
+    // this.packery.layout();
   }
 
+    ngAfterViewInit() {
+      console.log(this.grid.nativeElement);
+
+      var packeryOptions = {
+        itemSelector: '.grid-item',
+        // TODO: Remove rowHeight and columnWidth eventually
+        columnWidth: 100,
+        rowHeight: 100,
+        originLeft: true,
+        originTop: true,
+        resize: false
+      }
+
+      var packery = new Packery(this.grid.nativeElement, packeryOptions);
+
+      packery.getItemElements().forEach(function(itemElem) {
+        var draggedElement = new Draggabilly(itemElem);
+        // TODO: packery must be local
+        packery.bindDraggabillyEvents(draggedElement);
+      });
+
+      packery.on('dragItemPositioned', function(draggedItem) {
+        packery.layout();
+      });
+    }
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
 }
