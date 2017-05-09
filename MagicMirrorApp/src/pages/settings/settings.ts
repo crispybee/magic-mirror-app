bb56e8b1975adff863bc859b6faa8b6e8743a47f
Jenshika Yoganathan <jyoganat@stud.hs-heilbronn.de>
2017-05-09 21:19:00 +0200

    added jsonservice to create jsonobjects

--- MagicMirrorApp/src/pages/settings/settings.ts
+++ MagicMirrorApp/src/pages/settings/settings.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { JsonService } from "../../services/jsonservice";
 
 @Component({
   selector: 'page-settings',
@@ -11,6 +12,14 @@ export class SettingsPage {
   items: Array<{ adress: string, message: string }> = [];
   numberOfDashButtons: number = 0;
   constructor(public navCtrl: NavController) {
+    // testing
+    JsonService.getInstance().addRowsForDesktop(1, {
+      "row": 3,
+      "left": { "grid": "traffic", "wide": true },
+      "middle": { "grid": "empty", "wide": false },
+      "right": { "grid": "weather" }
+    }
+    );
   }
 
   save() {
---
+++ MagicMirrorApp/src/services/jsonservice.ts
@@ -0,0 +1,75 @@
+export class JsonService {
+
+    jsonForDesktop: any = {
+        "one": [],
+        "two": [],
+        "three": []
+    };
+
+    jsonForDashButtons: any = {
+        "dashbuttons": [
+        ]
+    };
+
+    jsonForDashButtonsActive: any = {
+        "active": [
+        ]
+    };
+
+    jsonForTraffic: any = {};
+
+    jsonForWifi: any = {};
+
+static instance:JsonService;
+    static isCreating:Boolean = false;
+ 
+   
+    constructor() {
+        if (!JsonService.isCreating) {
+            throw new Error("You can't call new in Singleton instances! Call SingletonService.getInstance() instead.");
+        }
+    }
+
+    static getInstance() {
+        if (JsonService.instance == null) {
+            JsonService.isCreating = true;
+            JsonService.instance = new JsonService();
+            JsonService.isCreating = false;
+        }
+ 
+        return JsonService.instance;
+    }
+
+    addRowsForDesktop(desktopNumber: number, row: any) {
+        if (desktopNumber === 1) {
+            this.jsonForDesktop.one.push(row);
+        }
+        if (desktopNumber === 2) {
+            this.jsonForDesktop.two.push(row);
+        }
+        if (desktopNumber === 3) {
+            this.jsonForDesktop.three.push(row);
+        }
+        console.log(this.jsonForDesktop);
+    }
+
+    addDashButton(button: any) {
+        this.jsonForDashButtons.dashbuttons.push(button);
+        console.log(this.jsonForDashButtons);
+    }
+
+    addDashButtonActive(activeButton: any) {
+        this.jsonForDashButtonsActive.active.push(activeButton);
+        console.log(this.jsonForDashButtonsActive);
+    }
+
+    addTraffic(traffic: any) {
+        this.jsonForTraffic = traffic;
+        console.log(this.jsonForTraffic);
+    }
+
+    addWifi(wifiConfiguration: any) {
+        this.jsonForWifi = wifiConfiguration;
+        console.log(this.jsonForWifi);
+    }
+}
